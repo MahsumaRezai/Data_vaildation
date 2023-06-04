58bd3e9b790cbba72599220863caf56bfc998d89
Masoumeh Rezai <you@example.com>
2023-06-04 05:53:14 +0430

    work on Adduser

---
+++ src/componets/AddUser/AddUser.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const AddUser = () => {
+    return (
+        <Fragment>
+            <form>
+
+            </form>
+        </Fragment>
+    )
+
+}
+export default AddUser;
