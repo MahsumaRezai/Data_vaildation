ea8e144e6633d563b883ac2c2f197205a1829a80
Masoumeh Rezai <you@example.com>
2023-06-04 09:02:27 +0430

    work on input section

--- src/componets/AddUser/AddUser.js
+++ src/componets/AddUser/AddUser.js
@@ -10,10 +10,12 @@ const AddUser = () => {
     return (
         <Card >
             <form className={classes.form} onSubmit={AddUserHandler}>
-                <label htmlFor="Name" className={classes.label} >Name</label>
+                <label htmlFor="Name" className={classes.label} >Frist name</label>
                 <input type="text" className={classes.input}></input>
-                <label htmlFor="Name" className={classes.label} >Age</label>
-                <input type="number " className={classes.input}></input>
+                <label htmlFor="Name" className={classes.label} >Last name</label>
+                <input type="text " className={classes.input}></input>
+                <label htmlFor="Name" className={classes.label} >Password</label>
+                <input type="password " className={classes.input}></input>
                 <Button>send me</Button>
 
             </form>
