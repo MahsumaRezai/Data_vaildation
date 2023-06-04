de79255cb468135b712e13961625f8fcc4f38922
Masoumeh Rezai <you@example.com>
2023-06-04 06:53:56 +0430

    work on submit section

--- src/componets/AddUser/AddUser.js
+++ src/componets/AddUser/AddUser.js
@@ -3,9 +3,13 @@ import Card from "../UI/Card";
 import classes from './AddUser.module.css'
 
 const AddUser = () => {
+    const AddUserHandler = (event) => {
+        event.preventDefault()
+
+    }
     return (
         <Card >
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={AddUserHandler}>
                 <label htmlFor="Name" className={classes.label} >Name</label>
                 <input type="text" className={classes.input}></input>
                 <label htmlFor="Name" className={classes.label} >Age</label>
