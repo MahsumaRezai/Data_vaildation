87288268f4735c37afbdd4ce5b9d13318fcd7406
Masoumeh Rezai <you@example.com>
2023-06-04 09:20:12 +0430

    work on usestate

--- src/componets/AddUser/AddUser.js
+++ src/componets/AddUser/AddUser.js
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from './AddUser.module.css'
 
 const AddUser = () => {
-    const AddUserHandler = (event) => {
-        event.preventDefault()
+    const [entreName, setentrename] = useState('');
+    const [entreLastName, setenterLastName] = useState('');
+    const [entrePassword, setentrPassword] = useState('');
 
+    const AddUserHandler = (event) => {
+        event.preventDefault();
+        console.log(entreName, entreLastName, entrePassword)
     }
+    const nameEnterHandler = (event) => {
+        setentrename(event.target.value)
+    }
+
+    const lastEnterHandler = (event) => {
+        setenterLastName(event.target.value)
+    }
+    const passwordEnterHandler = (event) => {
+        setentrPassword(event.target.value)
+    }
+
     return (
         <Card >
             <form className={classes.form} onSubmit={AddUserHandler}>
                 <label htmlFor="Name" className={classes.label} >Frist name</label>
-                <input type="text" className={classes.input}></input>
+                <input type="text" className={classes.input} onChange={nameEnterHandler}></input>
                 <label htmlFor="Name" className={classes.label} >Last name</label>
-                <input type="text " className={classes.input}></input>
+                <input type="text " className={classes.input} onChange={lastEnterHandler}></input>
                 <label htmlFor="Name" className={classes.label} >Password</label>
-                <input type="password " className={classes.input}></input>
-                <Button>send me</Button>
+                <input type="password " className={classes.input} onChange={passwordEnterHandler}></input>
+                <Button type="submit">send me</Button>
+
 
             </form>
         </Card>
--- src/componets/UI/Button.js
+++ src/componets/UI/Button.js
@@ -1,7 +1,7 @@
 import classes from './Button.module.css'
 const Button = (props) => {
     return (
-        <button className={classes.btn} type={props.type || 'button'} onClick={props.onClick}>{props.children}</button>
+        <button className={classes.btn} type={props.type || 'submit'} onClick={props.onClick}>{props.children}</button>
     )
 }
 export default Button;
