3d6173a459ac46fc07622fe3b37c810c2956494b
Masoumeh Rezai <you@example.com>
2023-06-04 06:25:14 +0430

    work on input section

--- src/componets/AddUser/AddUser.js
+++ src/componets/AddUser/AddUser.js
@@ -3,12 +3,12 @@ import classes from './AddUser.module.css'
 
 const AddUser = () => {
     return (
-        <Card className={classes.input}>
-            <form>
-                <label htmlFor="Name" >Name</label>
-                <input type="text"></input>
-                <label htmlFor="Name" >Age</label>
-                <input type="number"></input>
+        <Card >
+            <form className={classes.form}>
+                <label htmlFor="Name" className={classes.label} >Name</label>
+                <input type="text" className={classes.input}></input>
+                <label htmlFor="Name" className={classes.label} >Age</label>
+                <input type="number " className={classes.input}></input>
                 <button>Send me</button>
 
             </form>
